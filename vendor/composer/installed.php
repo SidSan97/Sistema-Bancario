b1769057c3913b0cd64b353bfd4eba8438f0fa7b
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-08-03 20:16:06 -0300

    Instalando depedencias

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'trilha/wj',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ed9832f43bd8104232cbd914b2604a6dd0d93f8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'trilha/wj' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ed9832f43bd8104232cbd914b2604a6dd0d93f8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
