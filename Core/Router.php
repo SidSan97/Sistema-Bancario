58b6423130d4c2aa7286bc2bb571b49887fb5d0c
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-08-03 20:26:35 -0300

    Adição da rota

---
+++ Core/Router.php
@@ -0,0 +1,37 @@
+<?php
+namespace Core;
+
+class Router
+{
+    private $controller = 'Controller';
+    private $method = 'home';
+    private $param = [];
+
+    public function __construct()
+    {
+        $router = $this->url();
+
+        if(file_exists('App/Controllers/'.ucfirst($router[0]).'.php')):
+            $this->controller = $router[0];
+            unset($router[0]);
+        endif;
+
+        $class = "\\App\\Controllers\\". ucfirst($this->controller);
+        $obj   = new $class;
+
+        if(isset($router[1]) and method_exists($class, $router[1])):
+            $this->method = $router[1];
+            unset($router[1]);
+        endif;
+
+        $this->param = $router ? array_values($router) : [];
+
+        call_user_func_array([$obj, $this->method], $this->param);
+    }
+
+    private function url()
+    {
+        $parse_url = explode("/", filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $parse_url;
+    }
+}
